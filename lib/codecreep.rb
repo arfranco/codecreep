aabac40b2d94ad21a8c2d9e3600c7a867077bc98
Anthony Franco <arfranco@Anthonys-MacBook-Pro.local>
2015-05-28 07:30:13 -0400

    Initial homework commit

--- lib/codecreep.rb
+++ lib/codecreep.rb
@@ -5,6 +5,7 @@ require 'pry'
 require 'codecreep/init_db'
 require 'codecreep/github'
 require "codecreep/version"
+require "codecreep/user"
 
 
 module Codecreep
@@ -12,8 +13,41 @@ module Codecreep
     def initialize
       @github = Github.new
     end
+
+    def fetch
+    end
+
+    def analyze
+    end
+
+    def find_or_create_by
+      @user = @github.get_user(user_name)
+      User.find_or_create_by(login: @user["login"]) do |t|
+      t.name = @user["name"]
+      t.homepage = @user["homepage"]
+      t.company = @user["company"]
+      t.follower_count = @user["follower_count"] 
+      t.following_count = @user["following_count"]
+      t.repo_count = @user["repo_count"]
+    end
+
+    # prompt the user for input and user can input any number of 
+    # usernames and we store it in an array. map the array to get the user names
+
+    def prompt(question, validator)
+      puts question
+      input = gets.chomp
+      until input =~ validator
+        puts "Sorry, I didn't understand that."
+        puts question
+        input = gets.chomp
+      end
+      input
+    end
+
   end
+
 end
 
-# app = Codecreep::App.new
-# binding.pry
+app = Codecreep::App.new
+binding.pry
--- lib/codecreep/github.rb
+++ lib/codecreep/github.rb
@@ -5,5 +5,19 @@ module Codecreep
     include HTTParty
     base_uri 'https://api.github.com'
     basic_auth ENV['GH_USER'], ENV['GH_PASS']
+
+    def get_user(user_id)
+      self.class.get("/users/#{username}")
+    end
+
+    # different api calls similar to issues
+    # all get requests
+
+
+    # get all of the 10 users and put them into the database
+    # call to get user from Github
+    #GET /users/:username to put each of the ten users in the databse
+    #get all of the followers and all the people they are following
+    
   end
 end
